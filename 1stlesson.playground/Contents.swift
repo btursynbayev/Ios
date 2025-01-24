2f31d47aacc496b7dcc7e12dd6bfad8cc6e62d08
btursynbayev <tursynbayevBakdaulet@gmail.com>
2025-01-24 15:47:44 +0500

    lesson1

---
+++ 1stlesson.playground/Contents.swift
@@ -0,0 +1,42 @@
+//var userName = "Tim Cook"
+//let age = 62
+//
+//print("My name is \(userName), i am \(age) years old")
+//
+//
+
+//1st task
+
+//let firstString = "I can "
+//let secondString = "code!"
+//
+//let sentenceString = firstString + secondString
+//
+//print(sentenceString)
+
+//task2.1
+
+//var myAge: Int
+//let age = 18
+//myAge = age
+//print(myAge)
+
+//task2.2
+
+//let myAge = 18
+//let tenYears = 10
+//var myAgeInTenYears: Int
+//myAgeInTenYears = myAge + tenYears
+//
+//print(myAgeInTenYears)
+
+//task 3
+
+//import Foundation // нужно для использования функции sqrt
+//
+//let a = 8.0
+//let b = 6.0
+//var c: Double
+//c = (a * a) + (b * b)
+//var d: Double = sqrt(c)
+//print(d)
