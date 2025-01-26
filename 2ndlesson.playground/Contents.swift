747295ad29ad75a534fb2c48ea4104b2e252b291
btursynbayev <tursynbayevBakdaulet@gmail.com>
2025-01-26 09:49:51 +0500

    2

---
+++ 2ndlesson.playground/Contents.swift
@@ -0,0 +1,53 @@
+//task1
+//let number1: Float = 3.14
+//let number2: Float = 42.0
+//
+//var sumOfNums: Double
+//sumOfNums = Double(number1 + number2)
+//
+//print(sumOfNums)
+
+//task2
+//let numberOne = 7
+//let numberTwo = 3
+//
+//var result = numberOne / numberTwo
+//var remainder = numberOne % numberTwo
+//
+//print("При делении \(numberOne) на \(numberTwo) результат равен \(result), остаток равен \(remainder)")
+
+//task3
+//data of birth
+//let dayOfBirth = 1
+//let monhtOfBirth = 8
+//let yearOfBirth = 2006
+//
+////current data
+//let currentDay = 26
+//let currentMonth = 1
+//let currentYear = 2025
+//
+//let secondsInPerDay = 24 * 60 * 60
+//let daysInMonth = 30
+//let daysInYear = 365
+//
+//var daysFromBirth = (currentYear - yearOfBirth) * daysInYear
+//daysFromBirth += (currentMonth - monhtOfBirth) * daysInMonth
+//daysFromBirth += currentDay - dayOfBirth
+//
+//let secondsFromBirth = secondsInPerDay * daysFromBirth
+//let yearsFromBirth = secondsFromBirth / secondsInPerDay / daysInYear
+//let monthFromBirth = secondsFromBirth / secondsInPerDay / daysInMonth
+//
+//print("\(yearsFromBirth) years, \(monthFromBirth) months, \(daysFromBirth) days, \(secondsFromBirth) seconds have passed since my birth")
+
+//task4
+//import Foundation
+//
+//let number: Double = 1.0
+//
+//let sinValue = sin(number)
+//
+//let roundedValue = round(sinValue * 1000) / 1000
+//
+//print("Sin of the number \(number) is: \(roundedValue)")
